a60d561ea44b57a904503328b11ddc216b1bd287
Hack Reactor Student <student@hackreactor.com>
2017-07-11 15:26:41 -0700

    remove commented code in blinkyDancerSpec.js

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -6,6 +6,7 @@ describe('blinkyDancer', function() {
   beforeEach(function() {
     clock = sinon.useFakeTimers();
     blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    gifDancer = makeGifDancer(20, 30, timeBetweenSteps)
   });
 
   it('should have a jQuery $node object', function() {
@@ -13,23 +14,18 @@ describe('blinkyDancer', function() {
   });
 
   it('should have a step function that makes its node blink', function() {
-    sinon.spy(blinkyDancer.$node, 'toggle');//????????????
+    sinon.spy(blinkyDancer.$node, 'toggle');
     blinkyDancer.step();
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
 
-  it('should have a function that creates a chubbyDancer', function() {
-    expect(chubbyDancer.$node.toggle.called)to.be.true;
-  })
   describe('dance', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
-
+      // clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
-
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(2);
     });
