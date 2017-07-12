be7d5ced89fbe604f748ef690c40490db0d0054a
Hack Reactor Student <student@hackreactor.com>
2017-07-11 19:50:14 -0700

    Add line up functionality

--- src/init.js
+++ src/init.js
@@ -1,16 +1,35 @@
 $(document).ready(function() {
   window.dancers = [];
+  window.rightHalf = [];
+  window.leftHalf = [];
+  window.midPoint;
 
   $('.addDancerButton').on('click', function(event) {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
     var dancerMakerFunction = window[dancerMakerFunctionName];
-    console.log(window)
+    // console.log(window)
     var dancer =  dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('.dancers').append(dancer.$node);
+    window.dancers.push(dancer)
+  });
+
+  $('.goCrazy').on('click', function(event){
+    window.midPoint = Math.floor(window.dancers.length / 2);
+    window.rightHalf = window.dancers.slice(midPoint)
+    window.leftHalf = window.dancers.slice(0, midPoint)
+    window.dancers.forEach((item, index)=>{
+      let styleProperties = (index%2===0) ? {left : '90%'} : {left : '5%'};
+      item.$node.animate(styleProperties, 1250);
+    })
+  })
+
+  $('.danceOff').on('click', function(event) {
+    console.log(window.dancers[1].$node.css("left"))
+    window.dancers[1].stepCenter()
   });
 
 });
